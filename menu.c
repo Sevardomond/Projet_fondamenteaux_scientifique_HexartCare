5e978d172b4df06ea8de88aaac4af920fab56188
Ali ramdani <ali.ramdani.dz@viacesi.fr>
2018-11-10 16:31:04 +0100

    Create menu.c

---
+++ menu.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "menu.h"
+
+
+// Création de la fonction menu
+void menu()
+
+{
+    printf("                          ___________________________________________________________________ \n");
+    printf("                         |                                                                   |\n");
+    printf("                         |                                MENU                               |\n");
+    printf("                         |                       Choisissez une action :                     |\n");
+    printf("                         |___________________________________________________________________|\n");
+    printf("                         |                Entrez 0 pour allumer une LED au choix             |\n");
+    printf("                         |                Entrez 1 pour allumer toutes les LEDs              |\n");
+    printf("                         |    Entrez 2 pour allumer les LEDs une par une en mode chenille    |\n");
+    printf("                         |                 Entrez 3 pour allumer 1 LED sur 2                 |\n");
+    printf("                         |                 Entrez 4 pour allumer 1 LED sur 3                 |\n");
+    printf("                         |                 Entrez 5 pour allumer 1 LED sur 9                 |\n");
+    printf("                         |             Entrez 6 pour afficher le mode escargot               |\n");
+    printf("                         |                Entrez 7 pour afficher une spirale                 |\n");
+    printf("                         |        Entrez 8 pour afficher le mode barre de chargement         |\n");
+    printf("                         |                 Entrez 9 pour allumer une LED au hasard           |\n");
+    printf("                         |___________________________________________________________________|\n");
+}
+
+
+
