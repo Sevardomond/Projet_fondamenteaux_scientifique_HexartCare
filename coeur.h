8ed0cb1c0a38f4e0ff0c25a8a30ed86c5d8fceec
Ali ramdani <ali.ramdani.dz@viacesi.fr>
2018-11-10 16:28:44 +0100

    Create coeur.h

---
+++ coeur.h
@@ -0,0 +1,11 @@
+//prototype de fonction des modes d'allumage de LED's
+void choix();
+void allled();
+void chenilleune();
+void unesurdeux();
+void unesurtrois();
+void unesurneuf();
+void escargot();
+void spirale();
+void chargement();
+void hasard();
