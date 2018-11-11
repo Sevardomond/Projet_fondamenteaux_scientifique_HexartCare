a15f7ec114765c08ad4dc7d5a96d7c2547e7df24
Ali ramdani <ali.ramdani.dz@viacesi.fr>
2018-11-11 02:11:06 +0100

    Update menu.c

--- menu.c
+++ menu.c
@@ -6,22 +6,20 @@
 void menu()
 
 {
+
+    printf("                                                          MENU                                \n");
     printf("                          ___________________________________________________________________ \n");
-    printf("                         |                                                                   |\n");
-    printf("                         |                                MENU                               |\n");
-    printf("                         |                       Choisissez une action :                     |\n");
-    printf("                         |___________________________________________________________________|\n");
-    printf("                         |                Entrez 0 pour allumer une LED au choix             |\n");
-    printf("                         |                Entrez 1 pour allumer toutes les LEDs              |\n");
-    printf("                         |    Entrez 2 pour allumer les LEDs une par une en mode chenille    |\n");
-    printf("                         |                 Entrez 3 pour allumer 1 LED sur 2                 |\n");
-    printf("                         |                 Entrez 4 pour allumer 1 LED sur 3                 |\n");
-    printf("                         |                 Entrez 5 pour allumer 1 LED sur 9                 |\n");
-    printf("                         |             Entrez 6 pour afficher le mode escargot               |\n");
-    printf("                         |                Entrez 7 pour afficher une spirale                 |\n");
-    printf("                         |        Entrez 8 pour afficher le mode barre de chargement         |\n");
-    printf("                         |                 Entrez 9 pour allumer une LED au hasard           |\n");
-    printf("                         |___________________________________________________________________|\n");
+    printf("                                          Entrez 0 pour allumer une LED au choix              \n");
+    printf("                                          Entrez 1 pour allumer toutes les LEDs               \n");
+    printf("                              Entrez 2 pour allumer les LEDs une par une en mode chenille     \n");
+    printf("                                          Entrez 3 pour allumer 1 LED sur 2                   \n");
+    printf("                                           Entrez 4 pour allumer 1 LED sur 3                  \n");
+    printf("                                           Entrez 5 pour allumer 1 LED sur 9                  \n");
+    printf("                                       Entrez 6 pour afficher le mode escargot                \n");
+    printf("                                          Entrez 7 pour afficher une spirale                  \n");
+    printf("                                  Entrez 8 pour afficher le mode barre de chargement          \n");
+    printf("                                           Entrez 9 pour allumer une LED au hasard            \n");
+
 }
 
 
